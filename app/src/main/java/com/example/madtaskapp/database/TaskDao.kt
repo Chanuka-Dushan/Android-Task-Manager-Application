62d14fe3f9cb83c22baaed0acb643a0520abdd48
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-05-10 13:51:01 +0530

    database  and queries updated

---
+++ app/src/main/java/com/example/madtaskapp/database/TaskDao.kt
@@ -0,0 +1,29 @@
+package com.example.madtaskapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.madtaskapp.model.Task
+
+
+@Dao
+interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("SELECT * FROM TASKS ORDER BY id DESC")
+    fun getAllTasks():LiveData<List<Task>>
+    @Query("SELECT * FROM TASKS WHERE taskTitle LIKE:query OR taskDesc LIKE :query")
+    fun searchTasks(query:String?):LiveData<List<Task>>
+
+}
